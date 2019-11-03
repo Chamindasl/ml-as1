99b7f0c2033debd4637f8ea39c402fb18a5e0629
Chaminda Amarasinghe <camara@SIDERO.Athlone.Local>
2019-11-03 17:32:48 +0000

    violin plots for price group by room type and neighbourhood group

--- main/main.py
+++ main/main.py
@@ -1,12 +1,13 @@
-from analytics.common.multi_graph import dist_plot, pie_plot, group_bar_plot
+from analytics.common.multi_graph import dist_plot, pie_plot, group_bar_plot, violin_plot
 from analytics.common.multi_graph import scatter_plot
-from const import F_PRICE, F_NUMERIC_FIELDS, F_ALL_FIELDS, F_ROOM_TYPE_ID, F_NEIGHBOURHOOD_GROUP_ID, F_NEIGHBOURHOOD_ID
+from const import F_PRICE, F_NUMERIC_FIELDS, F_ALL_FIELDS, F_ROOM_TYPE_ID, F_NEIGHBOURHOOD_GROUP_ID, F_NEIGHBOURHOOD_ID, \
+    F_AVAILABILITY_365
 from db.write import write
 from db.init.init import create_all_tables
 from db.read import read
 from file.file_read import process_data_file
 from utils.data import vertical_slice_all_data, summary, filter_by_index, group_count_list, group_by, \
-    two_group_count_list
+    two_group_count_list, vertical_slice_data
 from utils.print import print_summary
 
 ab_data_to_db = process_data_file()
@@ -51,8 +52,10 @@ dist_plot(
 
 # scatter_plot(vertical_slice_ab_data_p_lt3_500, F_ALL_FIELDS, True)
 
-group_count_list_by_room_type = group_count_list(group_by(ab_data_p_lte_500, [F_ROOM_TYPE_ID[1]]))
-group_count_list_by_neighbourhood_grp_id = group_count_list(group_by(ab_data_p_lte_500, [F_NEIGHBOURHOOD_GROUP_ID[1]]))
+group_by_room_type = group_by(ab_data_p_lte_500, [F_ROOM_TYPE_ID[1]])
+group_count_list_by_room_type = group_count_list(group_by_room_type)
+group_by_neighbourhood_grp = group_by(ab_data_p_lte_500, [F_NEIGHBOURHOOD_GROUP_ID[1]])
+group_count_list_by_neighbourhood_grp_id = group_count_list(group_by_neighbourhood_grp)
 
 pie_plot(group_count_list_by_room_type)
 pie_plot(group_count_list_by_neighbourhood_grp_id)
@@ -71,4 +74,9 @@ group_bar_plot(group_count_list_by_room_type_and_neighbourhood_grp_id)
 group_count_list_by__neighbourhood_grp_id_and_room_type = two_group_count_list(group_by(ab_data_p_lte_500,
                                                                                         [F_NEIGHBOURHOOD_GROUP_ID[1],
                                                                                          F_ROOM_TYPE_ID[1]]))
-group_bar_plot(group_count_list_by__neighbourhood_grp_id_and_room_type)
+
+price_group_by_room_type = {k: vertical_slice_data(v, F_PRICE[1]) for k, v in group_by_room_type.items()}
+violin_plot(price_group_by_room_type)
+
+price_group_by_neighbourhood_grp_id = {k: vertical_slice_data(v, F_PRICE[1]) for k, v in group_by_neighbourhood_grp.items()}
+violin_plot(price_group_by_neighbourhood_grp_id)
--- main/main_test.py
+++ main/main_test.py
@@ -1,33 +1,20 @@
-from analytics.common.multi_graph import dist_plot, group_bar_plot, pie_plot
+from analytics.common.multi_graph import dist_plot, pie_plot, group_bar_plot, violin_plot
 from analytics.common.multi_graph import scatter_plot
-from const import F_PRICE, F_NUMERIC_FIELDS, F_ALL_FIELDS, F_ROOM_TYPE_ID, F_NEIGHBOURHOOD_GROUP_ID
+from const import F_PRICE, F_NUMERIC_FIELDS, F_ALL_FIELDS, F_ROOM_TYPE_ID, F_NEIGHBOURHOOD_GROUP_ID, F_NEIGHBOURHOOD_ID, \
+    F_AVAILABILITY_365
 from db.write import write
 from db.init.init import create_all_tables
 from db.read import read
 from file.file_read import process_data_file
-from utils.data import vertical_slice_all_data, summary, filter_by_index, group_by, two_group_count_list, \
-    group_count_list
+from utils.data import vertical_slice_all_data, summary, filter_by_index, group_count_list, group_by, \
+    two_group_count_list, vertical_slice_data
 from utils.print import print_summary
 
+# ab_data_to_db = process_data_file()
+# create_all_tables()
+# write.insert_all_data(ab_data_to_db)
 ab_data = read.read_ab_data()
-# vertical_slice_all_data_list = vertical_slice_all_data(ab_data)
-#
-# print_summary(summary(ab_data, F_NUMERIC_FIELDS))
-# # scatter_plot(vertical_slice_all_data_list, F_ALL_FIELDS)
-# # scatter_plot(vertical_slice_all_data_list, F_ALL_FIELDS, True)
-#
-ab_data_p_lte_500 = filter_by_index(ab_data, F_PRICE, '<=', 500)
-vertical_slice_ab_data_p_lt3_500 = vertical_slice_all_data(ab_data_p_lte_500)
-print_summary(summary(ab_data_p_lte_500, [F_PRICE]))
-
-from analytics.common.multi_graph import group_bar_plot
-from utils.data import two_group_count_list
-
-count_list = two_group_count_list(group_by(ab_data_p_lte_500, [F_ROOM_TYPE_ID[1], F_NEIGHBOURHOOD_GROUP_ID[1]]))
-print(count_list)
-group_bar_plot(count_list)
-
-count_list = group_count_list(group_by(ab_data_p_lte_500, [F_ROOM_TYPE_ID[1]]))
-pie_plot(count_list)
-count_list.insert(1, [""])
-group_bar_plot(count_list)
+b = group_by(ab_data, [F_ROOM_TYPE_ID[1]])
+c = {i: vertical_slice_data(j, F_PRICE[1]) for i,j in b.items()}
+vertical_slice_all_data_list = vertical_slice_all_data(ab_data)
+violin_plot(c)
