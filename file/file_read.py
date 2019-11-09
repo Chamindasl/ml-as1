59d16c40054b0c8a66b5113715ddd450675a4dd8
Chaminda Amarasinghe <camara@SIDERO.Athlone.Local>
2019-11-09 14:58:51 +0000

    unit testing for file read

--- file/file_read.py
+++ file/file_read.py
@@ -1,9 +1,4 @@
-__room_types = []
-__neighbourhood_groups = []
-__neighbourhoods = []
-__ab_data = []
-__ab_headers = []
-__ab_skipped = []
+from exceptions.file_exceptions import FileNotReadableError
 
 
 def process_data_file(file="../data/AB_NYC_2019.csv"):
@@ -19,20 +14,34 @@ def process_data_file(file="../data/AB_NYC_2019.csv"):
         4. Assign 0 for missing Review per Month
 
     :param file: input file
+    :raise FileNotReadableError: When file not found or not readable
     :return: dictionary which contains room_types, neighbourhood_groups, neighbourhoods, headers, data, skipped data
     """
-    with open(file, encoding="utf8") as data_file:
-        read_header = False
-        for line in data_file:
-            line = line.strip()
-            comma_separated_line = line.split(",")
-            if read_header:
-                if len(comma_separated_line) == 16:
-                    process_line__(comma_separated_line)
-                else:
-                    __ab_skipped.append(comma_separated_line)
-
-        return build_dto__()
+    data_items = {
+        "room_types": [],
+        "neighbourhood_groups": [],
+        "neighbourhoods": [],
+        "ab_headers": [],
+        "ab_data": [],
+        "ab_skipped": []
+    }
+    try:
+        with open(file, encoding="utf8") as data_file:
+            read_header = False  # has header red
+            for line in data_file:
+                line = line.strip()
+                comma_separated_line = line.split(",")
+                if read_header:  # reading normal data row
+                    if len(comma_separated_line) == 16:  # there should be exactly 16 data items
+                        process_line__(comma_separated_line, data_items)  # process line
+                    else:
+                        data_items["ab_skipped"].append(comma_separated_line)  # skipped otherwise
+                else:  # first row as a header
+                    data_items["ab_headers"].append(comma_separated_line)
+                    read_header = True
+            return data_items
+    except:
+        raise FileNotReadableError
 
 
 def add_keys_with_idx__(a_list: list, value):
@@ -43,40 +52,41 @@ def add_keys_with_idx__(a_list: list, value):
     :return: if value in list with id then return id else new id
     """
     for k, v in a_list:
-        if v == value:
-            return k
+        if v == value:  # if value found
+            return k  # returned index
     else:
-        list_len = len(a_list) + 1
-        a_list.append((list_len, value))
-        return list_len
+        list_len = len(a_list) + 1  # current len + 1 would be next index for new item
+        a_list.append((list_len, value))  # append to list
+        return list_len  # new index
 
 
-def process_line__(one_line):
+def process_line__(one_line, data_items):
     """
     Process one row, convert cell value to correct type  and add to ab_data as a tuple
     """
     line_id, name, host_id, host_name, neighbourhood_group, neighbourhood, latitude, longitude, room_type, price, \
-        minimum_nights, number_of_reviews, last_review, reviews_per_month, calculated_host_listings_count, \
-        availability_365 = one_line
-    room_type_id = add_keys_with_idx__(__room_types, room_type)
-    neighbourhood_group_id = add_keys_with_idx__(__neighbourhood_groups, neighbourhood_group)
-    neighbourhood_id = add_keys_with_idx__(__neighbourhoods, neighbourhood)
-    reviews_per_month = float_or_0__(reviews_per_month)
-    __ab_data.append((
-                      name,
-                      host_id,
-                      room_type_id,
-                      neighbourhood_group_id,
-                      neighbourhood_id,
-                      float(latitude),
-                      float(longitude),
-                      int(price),
-                      int(minimum_nights),
-                      int(number_of_reviews),
-                      last_review,
-                      float(reviews_per_month),
-                      int(calculated_host_listings_count),
-                      int(availability_365)))
+    minimum_nights, number_of_reviews, last_review, reviews_per_month, calculated_host_listings_count, \
+    availability_365 = one_line  # read each item
+    room_type_id = add_keys_with_idx__(data_items["room_types"], room_type)  # replace room type with its index
+    # replace ng with idx
+    neighbourhood_group_id = add_keys_with_idx__(data_items["neighbourhood_groups"], neighbourhood_group)
+    neighbourhood_id = add_keys_with_idx__(data_items["neighbourhoods"], neighbourhood)  # replace n with ids
+    reviews_per_month = float_or_0__(reviews_per_month)  # cleansing reviews_per_month
+    data_items["ab_data"].append((
+        name,
+        host_id,
+        room_type_id,
+        neighbourhood_group_id,
+        neighbourhood_id,
+        float(latitude),
+        float(longitude),
+        int(price),
+        int(minimum_nights),
+        int(number_of_reviews),
+        last_review,
+        float(reviews_per_month),
+        int(calculated_host_listings_count),
+        int(availability_365)))
 
 
 def float_or_0__(value):
@@ -89,18 +99,3 @@ def float_or_0__(value):
         return float(value)
     except ValueError:
         return 0
-
-
-def build_dto__():
-    """
-    Build the final dto dictionary which has room_types, neighbourhood_groups, ab_headers, ab_data, ab_skipped
-    :return:
-    """
-    return {
-        "room_types": __room_types,
-        "neighbourhood_groups": __neighbourhood_groups,
-        "neighbourhoods": __neighbourhoods,
-        "ab_headers": __ab_headers,
-        "ab_data": __ab_data,
-        "ab_skipped": __ab_skipped,
-    }
---
+++ file/test_file_read.py
@@ -0,0 +1,74 @@
+from unittest import TestCase
+
+from exceptions.file_exceptions import FileNotReadableError
+from file.file_read import process_data_file
+
+HEADERS = [['id',
+            'name',
+            'host_id',
+            'host_name',
+            'neighbourhood_group',
+            'neighbourhood',
+            'latitude',
+            'longitude',
+            'room_type',
+            'price',
+            'minimum_nights',
+            'number_of_reviews',
+            'last_review',
+            'reviews_per_month',
+            'calculated_host_listings_count',
+            'availability_365']]
+
+
+class TestData(TestCase):
+
+    def test_process_data_file_10_lines(self):
+        data = process_data_file("../data/AB_NYC_10_lines.csv")
+        self.assertEqual([(1, 'Private room'), (2, 'Entire home/apt')], data["room_types"])
+        self.assertEqual([(1, 'Brooklyn'), (2, 'Manhattan')], data["neighbourhood_groups"])
+        self.assertEqual(10, len(data["neighbourhoods"]))
+        self.assertEqual(HEADERS, data["ab_headers"])
+        self.assertEqual(10, len(data["ab_data"]))
+        self.assertEqual(0, len(data["ab_skipped"]))
+
+    def test_process_data_file_10_lines_2_skip(self):
+        data = process_data_file("../data/AB_NYC_10_lines_2_skip.csv")
+        self.assertEqual([(1, 'Private room'), (2, 'Entire home/apt')], data["room_types"])
+        self.assertEqual([(1, 'Brooklyn'), (2, 'Manhattan')], data["neighbourhood_groups"])
+        self.assertEqual(8, len(data["neighbourhoods"]))
+        self.assertEqual(HEADERS, data["ab_headers"])
+        self.assertEqual(8, len(data["ab_data"]))
+        self.assertEqual(('Clean & quiet apt home by the park',
+                          '2787',
+                          1,  # id of Private room
+                          1,  # id of Brooklyn
+                          1,  # id of Kensington
+                          40.64749,
+                          -73.97237,
+                          149,
+                          1,
+                          9,
+                          '2018-10-19',
+                          0.21,
+                          6,
+                          365), data["ab_data"][0])
+        self.assertEqual(2, len(data["ab_skipped"]))
+        self.assertEqual("5203", data["ab_skipped"][0][0])
+        self.assertEqual("5238", data["ab_skipped"][1][0])
+
+    def test_process_data_file_file_not_exist(self):
+        self.assertRaises(FileNotReadableError, process_data_file, "../data/A.csv")
+
+    def test_process_data_file_main_fle(self):
+        data = process_data_file()
+        self.assertEqual([(1, 'Private room'), (2, 'Entire home/apt'), (3, 'Shared room')], data["room_types"])
+        self.assertEqual([(1, 'Brooklyn'),
+                          (2, 'Manhattan'),
+                          (3, 'Queens'),
+                          (4, 'Staten Island'),
+                          (5, 'Bronx')], data["neighbourhood_groups"])
+        self.assertEqual(218, len(data["neighbourhoods"]))
+        self.assertEqual(HEADERS, data["ab_headers"])
+        self.assertEqual(41707, len(data["ab_data"]))
+        self.assertEqual(7373, len(data["ab_skipped"]))
